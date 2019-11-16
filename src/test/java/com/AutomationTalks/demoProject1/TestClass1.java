9496fb0374349325f94f51ec1c5ed838edca04b2
sreedhar <sreedharg2007@gmail.com>
2019-11-16 21:52:45 +0000

    updated test with browserstack related

--- src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
+++ src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
@@ -1,9 +1,14 @@
 package com.AutomationTalks.demoProject1;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,11 +17,22 @@ public class TestClass1 {
 
 	public WebDriver driver;
 	
+	public static final String USERNAME = "sreenaths1";
+	public static final String AUTOMATE_KEY = "pnKHmQxF61d7HGXtyUdY";
+	public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+	
 	@BeforeMethod
-	public void launchDriver()
+	public void launchDriver() throws MalformedURLException
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");
-		driver = new ChromeDriver();
+		DesiredCapabilities caps = new DesiredCapabilities();
+	    caps.setCapability("browser", "Firefox");
+	    caps.setCapability("browser_version", "60.0");
+	    caps.setCapability("os", "Windows");
+	    caps.setCapability("os_version", "7");
+	    caps.setCapability("resolution", "1024x768");
+	    
+	    driver = new RemoteWebDriver(new URL(URL), caps);
+		
 		driver.manage().timeouts().implicitlyWait(10,  TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
 	}
